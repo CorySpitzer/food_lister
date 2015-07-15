5e8eb135a3976a26ee8c7e340477d4aaf120201a
Jessica <Jessicaepdx@gmail.com>
2015-07-15 11:22:55 -0700

    food table centered

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,5 @@
 class FoodsController < ApplicationController
   def index
-    @foods = Food.order(:name).paginate(:page => params[:page], :per_page => 10)
+    @foods = Food.order(:name).paginate(:page => params[:page], :per_page => 7)
   end
 end
