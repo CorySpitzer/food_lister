3f8a57cee3649cdebd38435b488aba77035046be
Jessica <Jessicaepdx@gmail.com>
2015-07-15 12:31:05 -0700

    table sortable desc and asc by all name, serving size, or calories

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,5 @@
 class FoodsController < ApplicationController
   def index
-      @foods = Food.search(params[:search]).order(:name).paginate(:page => params[:page], :per_page => 7)
+      @foods = Food.search(params[:search]).order(params[:sort] + " " + params[:direction]).paginate(:page => params[:page], :per_page => 7)
   end
 end
