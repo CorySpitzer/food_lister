9aafbb5d6f5057be7bd7593dbcf4bc94fb6d908a
Jessica <Jessicaepdx@gmail.com>
2015-07-15 12:35:12 -0700

    default sorted by name ascending

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,15 @@
 class FoodsController < ApplicationController
   def index
-      @foods = Food.search(params[:search]).order(params[:sort] + " " + params[:direction]).paginate(:page => params[:page], :per_page => 7)
+
+    @foods = Food.search(params[:search]).order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 7)
   end
+  private
+  def sort_column
+    params[:sort] || "name"
+  end
+
+  def sort_direction
+    params[:direction] || "asc"
+  end
+
 end
