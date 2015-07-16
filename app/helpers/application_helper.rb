c1939a42b09d9feb9549b0108e89964fb432fa80
Jessica Engel <Jessicaepdx@gmail.com>
2015-07-15 17:14:47 -0700

    working links the old way

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def sortable(sort_type)
     title = sort_type.titleize
     direction = sort_type == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
-    link_to title, {:sort => sort_type, :direction => direction, :page => nil, :remote => true}
+    link_to title, params.merge(:sort => sort_type, :direction => direction, :page => nil)
   end
 
   def get_arrow(sort_type)
