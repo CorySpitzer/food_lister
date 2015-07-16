7e775b15f561c590386d92cd291ee2935bbe8f02
Jessica Engel & Cory Spitzer <student@epicodus.com>
2015-07-16 12:40:20 -0700

    Add delete functionality with ajax

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -37,6 +37,17 @@ class FoodsController < ApplicationController
     end
   end
 
+  def destroy
+    @food = Food.find params[:id]
+    @food.destroy
+    respond_to do |format|
+      format.html {redirect_to foods_path }
+      format.js {}
+    end
+    # get the food again:
+    @foods = Food.search(params[:search]).order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 7)
+  end
+
   private
   def sort_column
     Food.column_names.include?(params[:sort]) ? params[:sort] : "name"
