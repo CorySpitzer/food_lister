b462532fee0af51b088fdca0b90554ce150da7ce
Jessica Engel & Cory Spitzer <student@epicodus.com>
2015-07-16 16:13:16 -0700

    add a date to food and associate a user when adding with form on user page

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -21,11 +21,16 @@ class FoodsController < ApplicationController
   end
 
   def create
+    @user = current_user
     @food = Food.new(food_params)
+    if params[:date]
+      @food.date = params[:date]
+      @user.foods.push(@food)
+    end
     @foods = Food.search(params[:search]).order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 7)
     if @food.save
       respond_to do |format|
-        format.html {redirect_to foods_path }
+        format.html {redirect_to user_path(current_user) }
         format.js
       end
     else
